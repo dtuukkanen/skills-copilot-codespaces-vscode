a6d65a874fc843a0fe8aa8ecf30fb62a7f27f7ca
Daniel Tuukkanen <41307603+dtuukkanen@users.noreply.github.com>
2024-05-29 12:23:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,25 @@
+function skillsMember() {
+    var skills = [
+        "C++",
+        "Python",
+        "Java",
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "SQL",
+        "MongoDB",
+        "React",
+        "Node.js",
+        "Express",
+        "jQuery",
+        "Bootstrap",
+        "Sass",
+        "Git",
+        "Linux",
+        "Docker",
+        "AWS",
+        "Heroku",
+        "Firebase"
+    ];
+    return skills;
+}
